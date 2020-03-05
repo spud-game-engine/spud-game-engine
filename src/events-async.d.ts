2b6b486e7f7150c1792dd1fbe1de33b328e2fb44
Nathan Fritzler <nfblaster@live.com>
2020-03-04 23:20:39 -0700

    Tame most typoing issues with AsyncEventEmitters

--- src/events-async.d.ts
+++ src/events-async.d.ts
@@ -1,1 +1,14 @@
-declare module 'events-async';
+declare module 'events-async' {
+	import {EventEmitter} from 'events';
+	type options={
+		series:boolean
+		catch:boolean
+	}
+	type retPromise=Promise<boolean|void>;
+	export default class AsyncEventEmitter extends EventEmitter {
+		emit(error:Error):void
+		emit(type:string):retPromise
+		emit(op:options,type:string):retPromise
+		emit(event: string | symbol, ...args: any[]):boolean //This is wrong
+	}
+}
