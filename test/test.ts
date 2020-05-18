e2c0cdd052399246883a15b02b9adf778098ef78
Nathan Fritzler <nfblaster@live.com>
2020-05-17 18:04:44 -0600

    Clean up some unnneded stuff
    I can get back to this stuff later once it actually matters, and by then
    core should be better defined.

--- test/test.ts
+++ test/test.ts
@@ -3,7 +3,4 @@
 const {suite}=intern.getPlugin("interface.tdd")
 import core from './core'
 suite("core",core);
-//TODO: reevalute the entire platformer module
-//import platformer from './platformer'
-//suite("platformer",platformer)
 
